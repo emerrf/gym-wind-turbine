e29e01f39ff19116d9f23d9b6dfcb5f1983a102d
Emer Rodriguez Formisano <emer@emer.es>
2017-12-15 11:01:42 +0100

    Reduce the simulation time to 30s

--- gym_wind_turbine/__init__.py
+++ gym_wind_turbine/__init__.py
@@ -4,11 +4,11 @@ from gym.envs.registration import register
 register(
     id='WindTurbine-v0',
     entry_point='gym_wind_turbine.envs:WindTurbine',
-    max_episode_steps=int(60.0/(1.0/20)),  # 60s -> 1200 steps
+    max_episode_steps=int(30.0/(1.0/20)),  # 30s -> 600 steps
     kwargs={
         'env_settings': {
             'timestep': 1.0/20.0,
-            'duration': 60.0,
+            'duration': 30.0,
             'wind': {
                 'mode': 'constant',
                 'speed': 8.0,
